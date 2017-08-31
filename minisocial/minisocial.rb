b569375704b24b705d949c0fdc0f282dc9334938
carloszorro <csanchezzorro@gmail.com>
2017-08-30 20:12:02 -0500

    ruby_2

--- minisocial/minisocial.rb
+++ minisocial/minisocial.rb
@@ -4,5 +4,25 @@ class Post
 
   def initialize(opts = {})
     @body = opts[:body].to_s
-    @user = opts[:user]
   end
+
+end
+
+class User
+
+  attr_accessor
+
+  def initialize(opts_1 = {})
+    @email = opts_1[:email]
+    @password = opts_1[:password]
+    @nickname = opts_1[:nickname].to_s #to_s me deja un valor por defecto con ""
+    @posts = opts_1[:posts] ? opts_1[:posts] : []
+  end
+
+end
+user = User.new(
+  password: 123456,
+)
+
+#me recorre todo lo que contenga el user
+puts user.inspect
